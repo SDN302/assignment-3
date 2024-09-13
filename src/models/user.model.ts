cc8cc33bd7bd119d43bdf1f345f8b7d8e662071d
Minh Vương <hardingadonis@gmail.com>
2024-09-13 23:47:26 +0700

    fix: use hashed password to save in database

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,13 +2,13 @@ import mongoose, { Schema } from 'mongoose';
 
 interface IUser {
 	username: string;
-	password: string;
+	hashedPassword: string;
 	isAdmin?: boolean;
 }
 
 const UserSchema = new mongoose.Schema({
 	username: { type: Schema.Types.String, required: true },
-	password: { type: Schema.Types.String, required: true },
+	hashedPassword: { type: Schema.Types.String, required: true },
 	isAdmin: { type: Schema.Types.Boolean, required: false, default: false },
 });
 
